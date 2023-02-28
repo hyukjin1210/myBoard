064dbd7305422c6c0304c124dc2de752babeed83
hyukjin jang <chaos2061@naver.com>
2023-02-28 23:12:12 +0900

    나만의 작은 게시판 완성

--- src/main/java/com/sparta/myboard/dto/BoardRequestDto.java
+++ src/main/java/com/sparta/myboard/dto/BoardRequestDto.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 public class BoardRequestDto {
     private String title;
     private String writer;
+    private String password;
     private String contents;
 
 
-    public BoardRequestDto(String title, String writer, String contents) {
-        this.title = title;
-        this.writer = writer;
-        this.contents = contents;
-    }
+//    public BoardRequestDto(String title, String writer, String password, String contents) {
+//        this.title = title;
+//        this.writer = writer;
+//        this.password = password;
+//        this.contents = contents;
+//    }
 }
--- src/main/java/com/sparta/myboard/service/BoardService.java
+++ src/main/java/com/sparta/myboard/service/BoardService.java
@@ -21,9 +21,15 @@ public class BoardService {
 //        this.boardRepository = boardRepository;
 //    }
 
+//    @Transactional
+//    public Board InsertBoard(String title, String writer, String password, String contents) {
+//        BoardRequestDto requestDto = new BoardRequestDto(title, writer, password, contents);
+//        Board board = new Board(requestDto);
+//        boardRepository.save(board);
+//        return board;
+//    }
     @Transactional
-    public Board InsertBoard(String title, String writer, String contents) {
-        BoardRequestDto requestDto = new BoardRequestDto(title, writer, contents);
+    public Board insertBoard (BoardRequestDto requestDto) {
         Board board = new Board(requestDto);
         boardRepository.save(board);
         return board;
@@ -35,8 +41,65 @@ public class BoardService {
     }
 
     @Transactional
-    public Board findBoardOne (Long id) {
+    public Board findBoardOne(Long id) {
 
         return boardRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public Long updateBoard (Long id , BoardRequestDto requestDto) throws Exception {
+        Board board = boardRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다."));
+//        if(board.getPassword().equals(requestDto.getPassword())) {
+//            requestDto.setTitle(requestDto.getTitle());
+//            requestDto.setWriter(requestDto.getWriter());
+//            requestDto.setContents(requestDto.getContents());
+//        }
+        if(!board.getPassword().equals(requestDto.getPassword())) {
+            throw new Exception("비밀번호가 일치하지 않습니다");
+        }
+        board.update(requestDto);
+        return board.getId();
+
+    }
+
+//    @Transactional
+//    public Long updateBoard (Long id, String password, BoardRequestDto requestDto) {
+//        Board board = boardRepository.findById(id).orElseThrow(() ->
+//                new NullPointerException("해당 아이디가 존재하지 않습니다."));
+//        if(password.equals(board.getPassword())) {
+//            board.setId(board.getId());
+//            board.setTitle(requestDto.getTitle());
+//            board.setWriter(requestDto.getWriter());
+//            board.setPassword(board.getPassword());
+//            board.setContents(requestDto.getContents());
+//        }
+//        board.update(requestDto);
+//        return id;
+//    }
+
+//    @Transactional
+//    public String updateBoard (String password, BoardRequestDto requestDto) {
+//        Board board = boardRepository.findByPassword(password).;
+//        board.updateBoard(requestDto);
+//        return null;
+//    }
+    /*
+    데이터 삭제부분
+    비밀번호를 url로 전달하지 않아도 비교가 가능해야 함.
+    해당 게시글 비밀번호의 비교를 그러면 어떻게 할 것인가?
+    * */
+    @Transactional
+    public String deleteBoard(Long id, String password) throws Exception {  //리턴을 받을 필요가 있을까..?
+        String msg = "";
+        Board board = boardRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다."));
+        if(!board.getPassword().equals(password)) {
+            throw new Exception("비밀번호가 일치하지 않습니다");
+        } else {
+            boardRepository.deleteById(id);
+            msg = "성공입니다.";
+        }
+        return msg;
     }
 }
